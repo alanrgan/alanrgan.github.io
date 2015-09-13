076f0679a1189d0d2668a32e1bce2f0d55ce01e4
Alan Gan <alanrgan@wirelessprvnat-172-17-138-185.near.illinois.edu>
2015-09-13 13:46:45 -0500

    formatting updated

--- js/cover.js
+++ js/cover.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 	$(".masthead-nav a").click(function() {
 		$(this).tab('show');
 	});
+
+	$("input").click(function() {
+		var inputValue = $(this).attr("value");
+		if(inputValue == "Github")
+			window.location.replace("http://www.github.com/alanrgan");
+		else if(inputValue == "Email")
+			window.location.replace("mailto:alanrgan@gmail.com");
+		return false;
+	});
 });
