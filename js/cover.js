805786cbabfdeb7a807609675bf20a6040940cab
Alan Gan <alanrgan@citeswpaurhprv-172-16-62-95.near.illinois.edu>
2015-09-15 16:47:33 -0500

    Updated contact me page with Font Awesome icons

--- js/cover.js
+++ js/cover.js
@@ -3,12 +3,20 @@ $(document).ready(function() {
 		$(this).tab('show');
 	});
 
-	$("input").click(function() {
-		var inputValue = $(this).attr("value");
-		if(inputValue == "Github")
+	$("button").click(function() {
+		var buttonId = $(this).attr('id');
+		if(buttonId == "githubbtn")
 			window.location.replace("http://www.github.com/alanrgan");
-		else if(inputValue == "Email")
+		else if(buttonId == "emailbtn")
 			window.location.replace("mailto:alanrgan@gmail.com");
+		else if(buttonId == "resumebtn")
+			window.location.replace("files/AlanGanResume.pdf");
+		else if(buttonId == "fbbtn")
+			window.location.replace("http://www.facebook.com/alantheazn");
+		else if(buttonId == "linkedinbtn")
+			window.location.replace("https://www.linkedin.com/pub/alan-gan/92/975/53a");
+		else if(buttonId == "goodreadsbtn")
+			window.location.replace("https://www.goodreads.com/user/show/1831090-alan-gan")
 		return false;
 	});
 });
