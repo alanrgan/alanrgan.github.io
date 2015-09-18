909ca0183a52ee9034b95984a57b22fff346381f
Alan Gan <alanrgan@citeswpaurhprv-172-16-48-66.near.illinois.edu>
2015-09-17 20:57:18 -0500

    Added buttons to Projects page and some bug fixes

--- js/cover.js
+++ js/cover.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 	$(".masthead-nav a").click(function() {
+
+		var tabid = $(this).text().toLowerCase();
+
+		// Go through each 'pane' and disable all buttons unless it is the active one
+		$(".pane").each(function() {
+			var tis = $(this);
+			if(tis.attr("id") != tabid)
+			{
+				$(this).children("button").each(function() {
+					$(this).css("display","none");
+				});
+			}
+			else
+			{
+				$(this).children("button").each(function() {
+					$(this).css("display","inline");
+				});
+			}
+		})
+
 		$(this).tab('show');
 	});
 
@@ -11,12 +31,14 @@ $(document).ready(function() {
 			window.location.replace("mailto:alanrgan@gmail.com");
 		else if(buttonId == "resumebtn")
 			window.location.replace("files/AlanGanResume.pdf");
-		else if(buttonId == "fbbtn")
-			window.location.replace("http://www.facebook.com/alantheazn");
 		else if(buttonId == "linkedinbtn")
 			window.location.replace("https://www.linkedin.com/pub/alan-gan/92/975/53a");
 		else if(buttonId == "goodreadsbtn")
 			window.location.replace("https://www.goodreads.com/user/show/1831090-alan-gan")
+		else if(buttonId == "treebtn")
+			window.location.replace("http://www.github.com/alanrgan/TreeVisualizer");
+		else if(buttonId == "ballgamebtn")
+			window.location.replace("http://www.github.com/alanrgan/BallJump");
 		return false;
 	});
 });
